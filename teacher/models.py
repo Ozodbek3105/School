d5e55a0a2e8a2bb87598d93a5e83160b7c7a2bd9
Diamondman51 <zshavkatov51@gmail.com>
2024-09-14 20:12:05 +0500

    on models creation stage

--- teacher/models.py
+++ teacher/models.py
@@ -4,5 +4,45 @@ from django.db import models
 # Create your models here.
 
 
-class MyUser(AbstractUser):
+class Teacher(AbstractUser):
     ...
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    lang = models.CharField(max_length=20)
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}. Language: {self.lang}'
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.BigIntegerField()
+    address = models.TextField()
+    group = models.ManyToManyField(Group)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name} {self.surname}'
+
+
+class Lesson(models.Model):
+    theme = models.CharField(max_length=255)
+    date = models.DateField()
+    description = models.CharField(max_length=255)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    file = models.FileField(upload_to=f"lessons/{group.name}",)
+
+
+class Score(models.Model):
+    st_name = models.CharField(Student)
+    created_at = ...
+
+l = Lesson()
+l.objects.filter(group__student_set)
