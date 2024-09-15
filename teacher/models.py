23a0757df85f6687b845388f76fc5ac7453ee0ea
Diamondman51 <zshavkatov51@gmail.com>
2024-09-15 21:48:43 +0500

    Teacher model changed

--- teacher/models.py
+++ teacher/models.py
@@ -1,11 +1,64 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
 
-class Teacher(AbstractUser):
-    ...
+class TeacherManager(BaseUserManager):
+
+    def create(self, first_name, last_name, surname, email, phone, address, password=None):
+        if not email:
+            raise ValueError('Invalid email')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            first_name=first_name,
+            last_name=last_name,
+            surname=surname,
+            phone=phone,
+            address=address
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, first_name, last_name, email, password):
+        if not email:
+            raise ValueError('Email is required')
+
+        user = self.model(
+            first_name=first_name,
+            last_name=last_name,
+            email=email
+        )
+
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_admin = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class Teacher(AbstractBaseUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.BigIntegerField(unique=True)
+    address = models.TextField()
+
+    objects = TeacherManager()
+
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
 
 
 class Group(models.Model):
@@ -44,4 +97,11 @@ class Score(models.Model):
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     mark = models.IntegerField(null=False)
-    created_at = ...
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+    was_present = models.BooleanField(default=True)
