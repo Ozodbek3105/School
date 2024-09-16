11265eda73f788012b632214d47b52016ec737b6
Diamondman51 <zshavkatov51@gmail.com>
2024-09-17 01:00:15 +0500

    added fields: gender, department, date_of_birth, education, profile_photo

--- teacher/models.py
+++ teacher/models.py
@@ -133,4 +133,4 @@ class Score_Attendance(models.Model):
 
 # class Attendance(models.Model):
 #     student = models.ForeignKey(Student, on_delete=models.CASCADE)
-#     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
+#     lesson = models.ForeignKey(Lesson,  on_delete=models.CASCADE)
