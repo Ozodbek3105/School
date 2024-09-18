9f3484bbd447d38f12ca8921dd920620ff4a4f9e
Diamondman51 <zshavkatov51@gmail.com>
2024-09-19 02:06:07 +0500

    Success: Professor CRUD completed

--- teacher/urls.py
+++ teacher/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from teacher.views import AddProfessorViewset, AddStudentViewset, AllProfessorsViewset, AllStudentsViewset, EditProfessorViewset, EditStudentViewset, ProfessorProfileViewset, StudentProfileViewset
+from teacher.views import AddProfessorViewset, AddStudentViewset, AllProfessorsViewset, AllStudentsViewset, DeleteProfessorViewset, EditProfessorViewset, EditStudentViewset, ProfessorProfileViewset, StudentProfileViewset
 
 urlpatterns = [
     path('all_professors/', AllProfessorsViewset.as_view(), name='all_professors'),
     path('add_professor/', AddProfessorViewset.as_view(), name='add_professor'),
-    path('edit_professor/', EditProfessorViewset.as_view(), name='edit_professor'),
+    path('edit_professor/<int:professor_id>', EditProfessorViewset.as_view(), name='edit_professor'),
     path('professor_profile/', ProfessorProfileViewset.as_view(), name='professor_profile'),
+    path('delete_professor/<int:professor_id>', DeleteProfessorViewset.as_view(), name='delete_professor'),
     path('all_students/', AllStudentsViewset.as_view(), name='all_students'),
     path('add_student/', AddStudentViewset.as_view(), name='add_student'),
     path('edit_student/', EditStudentViewset.as_view(), name='edit_student'),
--- teacher/views.py
+++ teacher/views.py
@@ -1,15 +1,23 @@
-from django.shortcuts import redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, redirect
 from django.template.response import TemplateResponse
 from django.views import View
 
-from teacher.forms import AddProfessorForm
+from teacher.forms import AddProfessorForm, EditProfessorForm
 
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
+
+User = get_user_model()
+
+
 class AllProfessorsViewset(View):
     def get(self, request):
-        return TemplateResponse(request, 'all-professors.html')
+        teachers = User.objects.all()
+        context = {"teachers": teachers}
+        return TemplateResponse(request, 'all-professors.html', context)
 
 
 class AddProfessorViewset(View):
@@ -32,13 +40,45 @@ class AddProfessorViewset(View):
 
 
 class EditProfessorViewset(View):
-    def get(self, request):
-        return TemplateResponse(request, 'edit-professor.html')
+    def get(self, request, professor_id):
+        teacher = get_object_or_404(User, id=professor_id)
+        form = AddProfessorForm(instance=teacher)
+        context = {
+            'form': form,
+            "teacher": teacher,
+        }
+        print(form.as_p())
+        print('caafaw ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd  ')
+        return TemplateResponse(request, 'edit-professor.html', context)
+    
+    def post(self, request, professor_id):
+        teacher = get_object_or_404(User, id=professor_id)
+        form = EditProfessorForm(request.POST, request.FILES, instance=teacher)
+        if form.is_valid():
+            form.save()
+            return redirect('all_professors')
+        context = {
+            'form': form,
+            'teacher': teacher,
+        }
+        print(form.errors)
+        return TemplateResponse(request, "edit-professor.html", context)
 
 
 class ProfessorProfileViewset(View):
     def get(self, request):
         return TemplateResponse(request, 'professor-profile.html')
+
+
+class DeleteProfessorViewset(View):
+    def get(self, request, professor_id=None):
+        if professor_id:
+            teacher = get_object_or_404(User, id=professor_id)
+            teacher.delete()
+            professor_id += 1
+            return HttpResponseRedirect(request.META.get("HTTP_REFERER", ""))
+            # return HttpResponseRedirect(request.path_info) # Does not work properly
+
 
 class AllStudentsViewset(View):
     def get(self, request):
