73ce4bbee52a7be1e254d1366faf5ee4a3f389db
Diamondman51 <zshavkatov51@gmail.com>
2024-09-20 04:23:34 +0500

    Bug fix: fixed bugs connected with tables

--- teacher/models.py
+++ teacher/models.py
@@ -68,7 +68,8 @@ class Teacher(AbstractBaseUser):
 
     objects = TeacherManager()
 
-    date_joined = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     last_login = models.DateTimeField(auto_now=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
