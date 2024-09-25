ca2a7448c6c7773ed635c64e8e1a5a6ba8338518
Diamondman51 <zshavkatov51@gmail.com>
2024-09-25 23:10:05 +0500

    changes on db: to the Teacher model added skills fiels

--- teacher/models.py
+++ teacher/models.py
@@ -76,7 +76,7 @@ class Teacher(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
-   
+    
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
 
